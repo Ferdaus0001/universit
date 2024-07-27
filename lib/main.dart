46e2320b6f576df9cc5aa6bbdff30427b896ecd4
Ferdaus0001 <ferdaushossan7788990@gmail.com>
2024-07-27 20:15:46 +0600

    add to appBar

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,27 @@ import 'package:flutter/material.dart';
  }
 
  class MyApp extends StatelessWidget{/// extends to MyApp
-  const MyApp({super.key});
+  const MyApp({super .key});
 
   @override
   Widget build(BuildContext context) {
 
- 
+    return const MaterialApp(home: HomeActivity());
   }
+
+ }
+ class HomeActivity extends StatelessWidget{
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+ return  Scaffold(
+        appBar: AppBar(title: const Text("MyApp"),),
+ body : const Center(child: Text("Hello World "))
+    );
+  }
+
+
 
  }
