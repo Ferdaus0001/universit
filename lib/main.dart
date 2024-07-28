de93876cab93549634663f896cbf6f525909f480
Ferdaus0001 <ferdaushossan7788990@gmail.com>
2024-07-28 08:01:51 +0600

    add  color and Theme

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
  return Scaffold(
-   appBar: AppBar(title:  const Text(" Home "),),
+   appBar: AppBar(title:  const Text(" Home. "),),
    body: const Center(child: Text(" Hello World "),),
  );
   }
