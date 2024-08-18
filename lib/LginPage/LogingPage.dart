b34a9bebc83e75f7c468f1087d33d8fe2d641362
Ferdaus0001 <ferdaushossan7788990@gmail.com>
2024-08-18 09:28:21 +0600

    add Screen on Other Screen

--- lib/LginPage/LogingPage.dart
+++ lib/LginPage/LogingPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:universit/body/bodyScreen.dart';
 
 class Lging extends StatefulWidget {
   const Lging({super.key});
@@ -12,58 +13,96 @@ class _LgingState extends State<Lging> {
   var Password = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return    Scaffold(
+      appBar: AppBar(
+        title: Text('Login',style: TextStyle(fontSize:22,color: Colors.blue),),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 11,
+        toolbarHeight: 60,
+        titleSpacing: 1,
+
+      ),
       body: Center(
-         child: Container(
-           width: 350,
-           child: Column(
-             mainAxisAlignment: MainAxisAlignment.center,
-             children: [
-               TextField(
-                 controller: Email,
-                 decoration: InputDecoration(
-                   hintText: 'Enter Gmail ',
-                   suffixIcon: Icon(Icons.email,color: Colors.blue,),
-                   focusedBorder: OutlineInputBorder(
-                     borderSide: BorderSide(
-                       color: Colors.blue,
-                       width: 2,
-                     )
-                   ),
-                   border: OutlineInputBorder(
-                     borderRadius: BorderRadius.circular(16),
-                   )
-                 ),
-               ),/// TestField_One_1
+      child: Container(
+        width: 350,
+        child: Center(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                maxLines: 1,
+
+                  decoration: InputDecoration(
+               suffixIcon: Icon(Icons.email_outlined,color: Colors.blue,),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 2,
+                      ),
+                  borderRadius: BorderRadius.circular(16)
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                        width: 1,
+                      )
+                    ),
+                    hintText: 'Email',
+                    label: const Text('Enter Email'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    )
+                  ),
+                ),
 
 
-                Container(height: 13,),
-               TextField(/// TextField Two
-                 obscureText: true,
-                 controller: Password,
-                 decoration: InputDecoration(
-                   suffixIcon: Icon(Icons.lock,color: Colors.blue,),
+               Container(height: 14,),
+                TextFormField(
 
-                     hintText: 'Enter  Password  ',
-                     focusedBorder: OutlineInputBorder(
-                         borderSide: BorderSide(
-                           color: Colors.blue,
-                           width: 2,
-                         )
-                     ),
-                     border: OutlineInputBorder(
-                       borderRadius: BorderRadius.circular(16),
-                     )
-                 ),
-               ),
-             ],
-           ),
-         ),
+                  obscureText: true,
+                  maxLines: 1,
 
+                  decoration: InputDecoration(
+
+                      suffixIcon: Icon(Icons.lock_outline,color: Colors.blue,),
+                      focusedBorder: OutlineInputBorder(
+
+                          borderSide: BorderSide(
+                            color: Colors.blue,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(16)
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 1,
+                          )
+                      ),
+                      hintText: 'Password ',
+                      label: const Text('Enter Password '),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      )
+                  ),
+                ),
+                  Container(height: 14,),
+                ElevatedButton(onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder:  (context) => Bookapp(), ));
+                 
+                }, child:Text('Lgin',style: TextStyle(fontSize: 23,color: Colors.blue),),
+
+                )
+              ],
+            ),
+          ),
         ),
-
-
-
+      ),
+    ),
     );
   }
 }
